27caae76287fb2468aeb8d5bba4cc6d1e2359de7
sinbieun <b135..!sun07>
2018-08-18 19:09:52 +0900

    2018-08-18 - 폰트 적용

--- app/src/main/java/com/fate/user/fateutil/layout/NoticeLayout.java
+++ app/src/main/java/com/fate/user/fateutil/layout/NoticeLayout.java
@@ -2,18 +2,23 @@ package com.fate.user.fateutil.layout;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.fate.user.fateutil.R;
 
 public class NoticeLayout extends LinearLayout {
+
+    LinearLayout currentLayout;
+    LayoutInflater li = null;
+
     public NoticeLayout(Context context) {
         super(context);
 
         String infService = context.LAYOUT_INFLATER_SERVICE;
-        LayoutInflater li = (LayoutInflater) context.getSystemService(infService);
-
-        LinearLayout currentLayout = (LinearLayout) li.inflate(R.layout.notice_main, null);
+        li = (LayoutInflater) context.getSystemService(infService);
+        currentLayout = (LinearLayout) li.inflate(R.layout.notice_main, null);
         addView(currentLayout);
     }
 }
